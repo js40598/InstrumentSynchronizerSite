f9239ac6e49382ebb64527107b271519ff26d2e2
unknown <szymonjarzab1997@gmail.com>
2021-03-08 22:47:00 +0100

    basic login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import transaction
 from django.shortcuts import render, redirect
@@ -10,11 +11,18 @@ from accounts.forms import RegisterForm
 
 
 class Login(View):
+    login_form = AuthenticationForm
+
     def get(self, request):
-        return render(request, 'accounts/login.html')
+        return render(request, 'accounts/login.html', {'form': self.login_form()})
 
     def post(self, request):
-        return render(request, 'accounts/login.html')
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'accounts/login.html', {'form': self.login_form, 'error': 'login failed'})
+        else:
+            login(request, user)
+            return redirect('landing')
 
 
 class Register(View):
