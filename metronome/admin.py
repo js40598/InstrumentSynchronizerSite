8fd3484e6102dc4db99b37b1153b4202bdd577f0
unknown <szymonjarzab1997@gmail.com>
2021-03-09 20:41:43 +0100

    metronome models

--- metronome/admin.py
+++ metronome/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from metronome.models import Metronome, Tick
 
 # Register your models here.
+
+admin.site.register(Metronome)
+admin.site.register(Tick)
--- metronome/models.py
+++ metronome/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Tick(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='ticks/')
+    values = models.TextField()
+
+
+class Metronome(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    frequency = models.PositiveIntegerField()
+    duration = models.PositiveIntegerField()
+    bpm = models.PositiveIntegerField()
+    tick = models.ForeignKey(Tick, on_delete=models.DO_NOTHING)
+    stereo = models.BooleanField(default=False)
