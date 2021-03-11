bf5e4183fe1306f0cd9cd66824c4640ab84f99ac
unknown <szymonjarzab1997@gmail.com>
2021-03-11 23:44:17 +0100

    generate metronome form generates file

--- metronome/views.py
+++ metronome/views.py
@@ -1,7 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+import numpy as np
+from scipy.io.wavfile import write
+import os
+
+from InstrumentSynchronizerSite import settings
+from InstrumentSynchronizerSite.utils import decode_samples
 from metronome.forms import MetronomeCreationForm, MetronomeSaveForm
 from metronome.models import Metronome, Tick
+from InstrumentSynchronizerSite.InstrumentSynchronizer.instrsyn.Metronome import Metronome as MetronomeFile
 
 # Create your views here.
 
@@ -39,6 +47,25 @@ class Generate(View):
                                                                   'duration': request.POST['duration'],
                                                                   'bpm': request.POST['bpm'],
                                                                   'tick': request.POST['tick']})
+
+                metronome_tick_values = np.array(decode_samples(Tick.objects.get(title=request.POST['tick']).values))
+
+                metronome_name = 'Metronome{}Hz{}sec{}bpm{}{}.wav'.format(request.POST['frequency'],
+                                                                          request.POST['duration'],
+                                                                          request.POST['bpm'],
+                                                                          request.POST.get('stereo', False),
+                                                                          request.POST['tick'])
+                if os.path.exists(metronome_name):
+                    pass
+                else:
+                    generated_metronome = MetronomeFile(frequency=int(request.POST['frequency']),
+                                                        duration=int(request.POST['duration']),
+                                                        bpm=int(request.POST['bpm']),
+                                                        tick_values=metronome_tick_values,
+                                                        stereo=request.POST.get('stereo', False))
+                    write(metronome_name, generated_metronome.frequency, generated_metronome.samples)
+                context['generated_metronome_url'] = metronome_name
+
             return render(request, 'metronome/generate.html', context)
         elif 'save_submit' in request.POST:
             # save metronome (to develop)
