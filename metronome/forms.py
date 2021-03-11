dabed401e736d0ce84f5187f3a90ae166804f67a
unknown <szymonjarzab1997@gmail.com>
2021-03-11 20:50:02 +0100

    save metronome form

--- metronome/forms.py
+++ metronome/forms.py
@@ -3,11 +3,23 @@ from .models import Metronome, Tick
 
 
 class MetronomeCreationForm(forms.ModelForm):
-    # TICK_CHOICES = [(tick.title, tick.title) for tick in Tick.objects.all()]
-    TICK_CHOICES = [('a', 'A'), ('b', 'B')]  # temporarily to show anything if Tick table is empty
+    TICK_CHOICES = [(tick.title, tick.title) for tick in Tick.objects.all()]
 
+    frequency = forms.ChoiceField(choices=Metronome.POSSIBLE_FREQUENCIES)
     tick = forms.ChoiceField(choices=TICK_CHOICES, widget=forms.RadioSelect)
 
     class Meta:
         model = Metronome
         fields = ['frequency', 'duration', 'bpm', 'tick', 'stereo']
+
+
+class MetronomeSaveForm(forms.ModelForm):
+    class Meta:
+        model = Metronome
+        fields = ['title', 'frequency', 'duration', 'bpm', 'tick', 'stereo']
+        widgets = {'frequency': forms.HiddenInput(),
+                   'duration': forms.HiddenInput(),
+                   'bpm': forms.HiddenInput(),
+                   'tick': forms.HiddenInput(),
+                   'stereo': forms.HiddenInput(),
+                   }
--- metronome/views.py
+++ metronome/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from metronome.forms import MetronomeCreationForm
+from metronome.forms import MetronomeCreationForm, MetronomeSaveForm
+from metronome.models import Metronome, Tick
 
 # Create your views here.
 
@@ -24,8 +25,31 @@ class Generate(View):
 
     def post(self, request):
         context = {
-            'creation_form': self.form(),
+            'creation_form': self.form(initial={'frequency': request.POST['frequency'],
+                                                'duration': request.POST['duration'],
+                                                'bpm': request.POST['bpm'],
+                                                'stereo': request.POST.get('stereo', False),
+                                                'tick': request.POST['tick']}),
         }
+
+        if 'creation_submit' in request.POST:
+            # generate metronome (to develop)
+            if request.user is not None:
+                context['save_form'] = MetronomeSaveForm(initial={'frequency': request.POST['frequency'],
+                                                                  'duration': request.POST['duration'],
+                                                                  'bpm': request.POST['bpm'],
+                                                                  'tick': request.POST['tick']})
+            return render(request, 'metronome/generate.html', context)
+        elif 'save_submit' in request.POST:
+            # save metronome (to develop)
+            metronome = Metronome(title=request.POST['title'],
+                                  user=request.user,
+                                  frequency=request.POST['frequency'],
+                                  duration=request.POST['duration'],
+                                  bpm=request.POST['bpm'],
+                                  tick=Tick.objects.get(title=request.POST['tick']),
+                                  stereo=request.POST['stereo'])
+
         return render(request, 'metronome/generate.html', context)
 
 
