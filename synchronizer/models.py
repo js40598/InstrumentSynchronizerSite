6d1fa376053e22b5a57edd540d1c88f7c4908070
unknown <szymonjarzab1997@gmail.com>
2021-03-20 06:46:32 +0100

    synchronizer models update

--- synchronizer/models.py
+++ synchronizer/models.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
-    creation_date = models.DateField(editable=False)
-    edition_date = models.DateTimeField()
+    description = models.TextField(max_length=200, blank=True)
+    creation_date = models.DateField(editable=False, blank=True)
+    edition_date = models.DateTimeField(blank=True)
 
     # set dates on creation or saving
     def save(self, *args, **kwargs):
@@ -19,9 +19,12 @@ class Project(models.Model):
         self.edition_date = datetime.now()
         return super(Project, self).save(*args, **kwargs)
 
+    class Meta:
+        unique_together = [['user', 'title']]
+
 
 class Recording(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, related_name='recordings', on_delete=models.CASCADE)
     file = models.FileField(upload_to='recordings/')
     instrument = models.CharField(max_length=100)
     identifier = models.CharField(max_length=100, blank=True)
